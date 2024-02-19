0cae6472e7f5fc90a89116dd464ff3a26e8fde5d
Lesley Burdock <lesleyburdock@Lesleys-MacBook-Air.local>
2024-02-19 14:35:04 +0000

    works

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-<script src="app.js" async></script>
-consoel.log("hello")
+
+
+console.log("hello")
+
+function getEmail(){
+    let email = document.querySelector("#email_adress");
+   let text = document.getElementById("message")
+   text.innerText=`Thanks for subscribing! A confirmation email has been sent to ${email.value}
+    Please open it and click the button inside to confirm your subscription.`;
+}
